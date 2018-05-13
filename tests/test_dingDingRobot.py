22782908a861654690d1f3f20c28e0fe5234db1d
pokercc <pokercc@sina.com>
2018-05-13 23:32:57 +0800

    去掉了部分单元测试

--- tests/test_dingDingRobot.py
+++ tests/test_dingDingRobot.py
@@ -5,6 +5,6 @@ from ..upload import DingDingRobot
 
 
 class TestDingDingRobot(TestCase):
-    def test_send_message(self):
+    def _test_send_message(self):
         access_token = os.getenv('ding_access_token')
         access_token and DingDingRobot(access_token).send_message('测试消息')
